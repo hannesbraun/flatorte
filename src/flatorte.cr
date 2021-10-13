19b04e03af9ca50325387832ec4db03e4535e474
Hannes Braun <hannesbraun@mail.de>
2021-10-13 23:31:40 +0200

    Remove module again

--- src/flatorte.cr
+++ src/flatorte.cr
@@ -4,140 +4,137 @@ require "hash"
 require "json"
 require "time"
 
-# TODO: Write documentation for `Flatorte`
-module Flatorte
-  VERSION = "0.1.0"
+VERSION = "0.1.0"
 
-  # TCOR_API = "https://tcor.mosad.xyz"
-  TCOR_API = "http://localhost:8080"
+# TCOR_API = "https://tcor.mosad.xyz"
+TCOR_API = "http://localhost:8080"
 
-  struct Lesson
-    getter subject, room, dstart, dend, remark
+struct Lesson
+  getter subject, room, dstart, dend, remark
 
-    def initialize(@subject : String, @room : String, @dstart : Time, @dend : Time, @remark : String)
-    end
+  def initialize(@subject : String, @room : String, @dstart : Time, @dend : Time, @remark : String)
   end
+end
 
-  def is_winter
-    month = Time.local.month
-    month >= 9 || month <= 2
-  end
+def is_winter
+  month = Time.local.month
+  month >= 9 || month <= 2
+end
 
-  def id_to_time(id)
-    case id
-    when 0
-      {8, 0}
-    when 1
-      {9, 45}
-    when 2
-      if is_winter
-        {11, 35}
-      else
-        {12, 0}
-      end
-    when 3
-      {14, 0}
-    when 4
-      {15, 45}
-    when 5
-      {17, 30}
+def id_to_time(id)
+  case id
+  when 0
+    {8, 0}
+  when 1
+    {9, 45}
+  when 2
+    if is_winter
+      {11, 35}
     else
-      {0, 0}
+      {12, 0}
     end
+  when 3
+    {14, 0}
+  when 4
+    {15, 45}
+  when 5
+    {17, 30}
+  else
+    {0, 0}
+  end
+end
+
+puts "Flatorte #{VERSION}"
+puts "Copyright 2021 Hannes Braun"
+puts "Powered by the citadel of Ricks"
+
+# Subcache for every course, containing a hash mapping the lessons to their week
+cache = Hash(String, Tuple(Array(Lesson), Time)).new
+
+server = HTTP::Server.new do |context|
+  resource = context.request.resource
+
+  if resource == "/"
+    context.response.content_type = "text/plain"
+    context.response.print "Flatorte #{VERSION}"
+    next
   end
 
-  puts "Flatorte #{VERSION}"
-  puts "Copyright 2021 Hannes Braun"
-  puts "Powered by the citadel of Ricks"
+  course = Path.new(resource).basename
 
-  # Subcache for every course, containing a hash mapping the lessons to their week
-  cache = Hash(String, Tuple(Array(Lesson), Time)).new
+  if course.includes?("/") || Path.new(resource).dirname != "/"
+    # Invalid path
+    context.response.status = HTTP::Status::NOT_FOUND
+    next
+  end
 
-  server = HTTP::Server.new do |context|
-    resource = context.request.resource
+  beginning_of_week = Time.local(Time::Location.load("Europe/Berlin")).at_beginning_of_week
+  unless cache.has_key?(course.upcase)
+    cache[course.upcase] = {Array(Lesson).new, Time.local - Time::Span.new(days: 1)}
+  end
 
-    if resource == "/"
-      context.response.content_type = "text/plain"
-      context.response.print "Flatorte #{VERSION}"
-      next
-    end
+  cache_age = (Time.local - cache[course.upcase][1])
+  if cache_age.hours > 2 || cache_age.days > 0
+    # Update cache
+    lessons = Array(Lesson).new
 
-    course = Path.new(resource).basename
+    (0...3).each do |week_index|
+      # TODO use a fiber here
 
-    if course.includes?("/") || Path.new(resource).dirname != "/"
-      # Invalid path
-      context.response.status = HTTP::Status::NOT_FOUND
-      next
-    end
+      tcor_url = "#{TCOR_API}/timetable?course=#{course}&week=#{week_index}"
+      timetable = JSON.parse(HTTP::Client.get(tcor_url).body)
+      week = timetable["meta"]["weekNumberYear"].as_i
 
-    beginning_of_week = Time.local(Time::Location.load("Europe/Berlin")).at_beginning_of_week
-    unless cache.has_key?(course.upcase)
-      cache[course.upcase] = {Array(Lesson).new, Time.local - Time::Span.new(days: 1)}
-    end
-
-    cache_age = (Time.local - cache[course.upcase][1])
-    if cache_age.hours > 2 || cache_age.days > 0
-      # Update cache
-      lessons = Array(Lesson).new
-
-      (0...3).each do |week_index|
-        # TODO use a fiber here
-
-        tcor_url = "#{TCOR_API}/timetable?course=#{course}&week=#{week_index}"
-        timetable = JSON.parse(HTTP::Client.get(tcor_url).body)
-        week = timetable["meta"]["weekNumberYear"].as_i
-
-        timetable["timetable"]["days"].as_a.each do |day|
-          timeslots = day["timeslots"]
-          timeslots.as_a.each do |timeslot|
-            timeslot.as_a.each do |lesson|
-              subject = lesson["lessonSubject"].as_s
-              if subject.empty?
-                next
-              end
-              room = lesson["lessonRoom"].as_s
-              remark = lesson["lessonRemark"].as_s
-              id = lesson["lessonID"].as_s.split('.')
-              day = id[0].to_i
-              time = id_to_time(id[1].to_i)
-              dstart = beginning_of_week + Time::Span.new(days: day, hours: time[0], minutes: time[1])
-              dend = dstart + Time::Span.new(hours: 1, minutes: 30)
-              lessons.push(Lesson.new(subject, room, dstart, dend, remark))
+      timetable["timetable"]["days"].as_a.each do |day|
+        timeslots = day["timeslots"]
+        timeslots.as_a.each do |timeslot|
+          timeslot.as_a.each do |lesson|
+            subject = lesson["lessonSubject"].as_s
+            if subject.empty?
+              next
             end
+            room = lesson["lessonRoom"].as_s
+            remark = lesson["lessonRemark"].as_s
+            id = lesson["lessonID"].as_s.split('.')
+            day = id[0].to_i
+            time = id_to_time(id[1].to_i)
+            dstart = beginning_of_week + Time::Span.new(days: day, hours: time[0], minutes: time[1])
+            dend = dstart + Time::Span.new(hours: 1, minutes: 30)
+            lessons.push(Lesson.new(subject, room, dstart, dend, remark))
           end
         end
-
-        beginning_of_week += Time::Span.new(days: 7)
       end
 
-      cache[course.upcase] = {lessons, Time.local}
+      beginning_of_week += Time::Span.new(days: 7)
     end
 
-    ics = String.build do |str|
-      str << "BEGIN:VCALENDAR\n"
-      str << "VERSION:2.0\n"
-      str << "CALSCALE:GREGORIAN\n"
-
-      cache[course.upcase][0].each do |lesson|
-        str << "BEGIN:VEVENT\n"
-        unless lesson.subject.empty?
-          str << "SUMMARY:#{lesson.subject}\n"
-        end
-        unless lesson.room.empty?
-          str << "LOCATION:#{lesson.room}\n"
-        end
-        str << "DTSTART;TZID=Europe/Berlin:#{lesson.dstart.to_s("%Y%m%d%H%M%S")}\n"
-        str << "DTEND;TZID=Europe/Berlin:#{lesson.dend.to_s("%Y%m%d%H%M%S")}\n"
-        str << "END:VEVENT\n"
-      end
-
-      str << "END:VCALENDAR\n"
-    end
-
-    context.response.content_type = "text/calendar"
-    context.response.print ics
+    cache[course.upcase] = {lessons, Time.local}
   end
 
-  puts "Listening on http://127.0.0.1:7453"
-  server.listen(7453)
+  ics = String.build do |str|
+    str << "BEGIN:VCALENDAR\n"
+    str << "VERSION:2.0\n"
+    str << "CALSCALE:GREGORIAN\n"
+
+    cache[course.upcase][0].each do |lesson|
+      str << "BEGIN:VEVENT\n"
+      unless lesson.subject.empty?
+        str << "SUMMARY:#{lesson.subject}\n"
+      end
+      unless lesson.room.empty?
+        str << "LOCATION:#{lesson.room}\n"
+      end
+      str << "DTSTART;TZID=Europe/Berlin:#{lesson.dstart.to_s("%Y%m%d%H%M%S")}\n"
+      str << "DTEND;TZID=Europe/Berlin:#{lesson.dend.to_s("%Y%m%d%H%M%S")}\n"
+      str << "END:VEVENT\n"
+    end
+
+    str << "END:VCALENDAR\n"
+  end
+
+  context.response.content_type = "text/calendar"
+  context.response.print ics
 end
+
+puts "Listening on http://127.0.0.1:7453"
+server.listen(7453)
