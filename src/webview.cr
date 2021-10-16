69806b7435501253d690930b538ab9fd4825d65a
Hannes Braun <hannesbraun@mail.de>
2021-10-17 01:34:15 +0200

    Don't crash if course list is not available

--- src/webview.cr
+++ src/webview.cr
@@ -5,7 +5,11 @@ class WebView
   @courses : Array(String)
 
   def initialize
-    @courses = courseList
+    @courses = begin
+      courseList
+    rescue
+      Array(String).new
+    end
   end
 
   ECR.def_to_s "src/webview.ecr"
